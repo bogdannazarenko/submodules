4b08d86b3983e26856facb44328e8f3a191b0b7f
Bogdan Nazarenko <bogdan.nazarenko@outlook.com>
2021-01-30 12:49:12 -0500

    cli

---
+++ cli.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bogdannazarenko/submodules/module1"
+)
+
+func main() {
+	fmt.Println(module1.Hi())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bogdannazarenko/submodules
 
 go 1.15
+
+require github.com/bogdannazarenko/submodules/module1 v0.2.1
